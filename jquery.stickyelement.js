0c4ea80a6bd2e536446e8d9e5820814e6c12433b
Enzo Martin <enzo.r.martin@gmail.com>
2012-10-12 03:08:20 +0200

    Fixed the issue with items not being seperated from each correctly, had to add a check for the parent element, making sure it only selects the closest parent if using a class

--- jquery.stickyelement.js
+++ jquery.stickyelement.js
@@ -1,7 +1,14 @@
 ;(function($, window, undefined) {
 
 	"use strict";
-
+	
+	/**
+	 * Sticky Element constructor
+	 * @param ele
+	 * @param par
+	 * @param options
+	 * @constructor
+	 */
 	var Sticky = function(ele, par, options) {
 		this.ele = ele;
 		this.par = par;
@@ -13,14 +20,16 @@
 		this.init();
 	};
 	
-	Sticky.prototype.Update = function(){
-		//This will handle any resizing of the container the sticky scroll is in andupdate the boundaries if necessary
-	};
-	
 	Sticky.prototype.init = function() {
 		this.SetBoundaries();
 		this.ele.css({'position':'relative'});
 		this.par.css({'position':'relative'});
+		this.MoveIt();
+	};
+	
+	Sticky.prototype.Update = function(){
+		//This will handle any resizing of the container the sticky scroll is in and update the boundaries if necessary
+		this.SetBoundaries();
 		this.MoveIt();
 	};
 	
@@ -39,6 +48,10 @@
 		this.yLimitBtm = this.yLimitTop + this.par.height() - this.ele.outerHeight();
 	};
 	
+	/**
+     * Update Stickied Elements' offset
+     * @param yOffset
+     */
 	Sticky.prototype.UpdateOffset = function(yOffset){
 		// This moves the item
 		if(this.animate){
@@ -57,47 +70,39 @@
 
 	$.fn.sticky = function(par,options) {
 		var method, args, ret = false;
-		if (!par){
-			par = $(document);
-		}
 		if (typeof options === "string") {
 			args = [].slice.call(arguments, 0);
 		}
-		if(typeof $(par) === 'object'){
-			par = $(par);
-		}
 		
-		if(typeof par === 'object'){
-			this.each(function() {
-				var self = $(this);
-				var instance = self.data("stickyInstance");
-				if (instance && options) {
-					if (typeof options === "object") {
-						ret = $.extend(instance.options, options);
-					} else if (options === "options") {
-						ret =  instance.options;
-					} else if (typeof instance[options] === "function") {
-						ret = instance[options].apply(instance, args.slice(1));
-					} else {
-						throw new Error("Sticky Element has no option/method named " + method);
-					}
+		this.each(function() {
+			var self = $(this);
+			var parent = par;
+			parent = (self.closest(parent))? self.closest(parent) : $(document);
+			var instance = self.data("stickyInstance");
+			
+			if (instance && options) {
+				if (typeof options === "object") {
+					ret = $.extend(instance.options, options);
+				} else if (options === "options") {
+					ret =  instance.options;
+				} else if (typeof instance[options] === "function") {
+					ret = instance[options].apply(instance, args.slice(1));
 				} else {
-					instance = new Sticky(self, par, options || {});
-					self.data("stickyInstance", instance);
-					$.stickies.push(instance);
+					throw new Error("Sticky Element has no option/method named " + method);
 				}
-			});
-		} else {
-			throw new Error('Parameter needs to be a jQuery object, or leave blank to target the Document');
-		}
+			} else {
+				instance = new Sticky(self, parent, options || {});
+				self.data("stickyInstance", instance);
+				$.stickies.push(instance);
+			}
+		});
 		return ret || this;
 	};
 	
 	// Update the position/offset changed on resize and move
 	$(window).on('resize',function(){
 		$.each($.stickies, function(){
-			this.SetBoundaries();
-			this.MoveIt();
+			this.Update();
 		});
 	});
 	
