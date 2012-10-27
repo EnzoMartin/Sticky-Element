0369c953b6a69d45fc64035163a5823fb6d0a220
Enzo Martin <enzo.r.martin@gmail.com>
2012-10-27 21:33:04 +0200

    Newest version, fixed up some errors with setting boundaries

--- jquery.stickyelement.js
+++ jquery.stickyelement.js
@@ -1,136 +1,141 @@
 ;(function($, window, undefined) {
 
-	"use strict";
+    "use strict";
 
-	/**
-	 * Sticky Element constructor
-	 * @param elm
-	 * @param par
-	 * @param options
-	 * @constructor
-	 */
-	var Sticky = function(elm, par, options) {
-		this.elm = elm;
-		this.par = par;
-		this._frozen = false;
-		this.options = $.extend({
-			animate: false,
-			animTime: 300
-		}, options);
-		this.init();
-	};
+    /**
+     * Sticky Element constructor
+     * @param elm
+     * @param par
+     * @param options
+     * @constructor
+     */
+    var Sticky = function(elm, par, options) {
+        this.element = elm;
+        this.parent = par;
+        this._frozen = false;
+        this.options = $.extend({
+            animate: false,
+            animTime: 300
+        }, options);
+        this.init();
+    };
 
-	Sticky.prototype.init = function() {
-		this.setBoundaries();
-		this.elm.css({'position': 'relative'});
-		this.par.css({'position': 'relative'});
-		this.moveIt();
-	};
+    Sticky.prototype.init = function() {
+        this.element.addClass("sticky-scroll");
+        this.update();
+    };
 
-	Sticky.prototype.update = function() {
-		//This will handle any resizing of the container the sticky scroll is in and update the boundaries if necessary
-		this.setBoundaries();
-		this.moveIt();
-	};
+    Sticky.prototype.update = function() {
+        //This will handle any resizing of the container the sticky scroll is in and update the boundaries if necessary
+        this.setBoundaries();
+        this.moveIt();
+    };
 
-	Sticky.prototype.moveIt = function() {
-		// This will decide whether to move the stickied item
-		var winOffset = window.pageYOffset;
-		if (winOffset >= this.yLimitTop && winOffset <= this.yLimitBtm) {
-			this.updateOffset(winOffset - this.yLimitTop);
-		} else if (winOffset < this.yLimitTop) {
-			this.updateOffset(0);
-		}
-	};
+    Sticky.prototype.moveIt = function() {
+        // This will decide whether to move the stickied item
+        var scrollTop = $(window).scrollTop();
+        var height = this.element.outerHeight(true);
+        var realStop = this._stop - height;
 
-	Sticky.prototype.setBoundaries = function() {
-		// This will set the boundaries the stickied item can move between and it's left position
-		this.xPos = this.elm.offset().left;
-		this.yLimitTop = this.par.offset().top;
-		var parHeight = this.par.height();
-		var offsetDiff = (this.elm.offset().top - this.elm.css('marginTop').replace('px','')) - this.yLimitTop;
-		if(offsetDiff === 0){
-			this.yLimitBtm = this.yLimitTop + parHeight - this.elm.outerHeight();
-		} else {
-			this.yLimitBtm = this.yLimitTop + parHeight - (this.elm.outerHeight() + offsetDiff);
-		}
-	};
+        if (this._parentHeight - this._offset > height) {
+            if (scrollTop >= this._start && scrollTop <= realStop) {
+                this.updateOffset(scrollTop - this._start);
+            } else if (scrollTop < this._start) {
+                this.updateOffset(0);
+            } else if (scrollTop > realStop) {
+                this.updateOffset(this._parentHeight - height - this._offset);
+            }
+        }
+    };
 
-	/**
-	 * Update Stickied Element's offset
-	 * @param yOffset
-	 */
-	Sticky.prototype.updateOffset = function(yOffset) {
-		// This moves the item
-		if (this.animate) {
-			this.elm.stop().animate({'top': yOffset}, this.animTime);
-		} else {
-			this.elm.css({'top': yOffset});
-		}
-	};
+    Sticky.prototype.setBoundaries = function() {
+        // This will set the boundaries the stickied item can move between and it's left position
+        this._offset = this.element.position().top;
+        this._start = this.parent.offset().top + this._offset;
+        this._parentHeight = this.parent.outerHeight();
+        this._stop = this._start + this._parentHeight - this._offset;
+    };
 
-	Sticky.prototype.toggleFreeze = function() {
-		// This will freeze the stickied item in place wherever it is
-		this._frozen = !this._frozen;
-	};
+    /**
+     * Update Stickied Element's offset
+     * @param yOffset
+     */
+    Sticky.prototype.updateOffset = function(yOffset) {
+        if (!this._lastPosition !== yOffset) {
+            // This moves the item
+            if (this.animate) {
+                this.element.stop().animate({
+                    'top': yOffset
+                }, this.animTime);
+            } else {
+                this.element.css('top', yOffset);
+            }
+            this._lastPosition = yOffset;
+        }
+    };
 
-	$.fn.sticky = function(par, options) {
-		var method, args, ret = false;
-		if (typeof options === "string") {
-			args = [].slice.call(arguments, 0);
-		}
+    Sticky.prototype.toggleFreeze = function() {
+        // This will freeze the stickied item in place wherever it is
+        this._frozen = !this._frozen;
+    };
 
-		this.each(function() {
-			var self = $(this);
-			var parent = par;
-			if (parent) {
-				parent = self.parent().closest(parent);
-			} else {
-				parent = self.parent();
-			}
-			var instance = self.data("stickyInstance");
+    $.fn.sticky = function(par, options) {
+        var method, args, ret = false;
+        if (typeof options === "string") {
+            args = [].slice.call(arguments, 0);
+        }
 
-			if (instance && options) {
-				if (typeof options === "object") {
-					ret = $.extend(instance.options, options);
-				} else if (options === "options") {
-					ret = instance.options;
-				} else if (typeof instance[options] === "function") {
-					ret = instance[options].apply(instance, args.slice(1));
-				} else {
-					throw new Error("Sticky Element has no option/method named " + method);
-				}
-			} else {
-				instance = new Sticky(self, parent, options || {});
-				self.data("stickyInstance", instance);
-				$.fn.sticky._instances.push(instance);
-			}
-		});
-		return ret || this;
-	};
+        this.each(function() {
+            var self = $(this);
+            var parent = par;
+            if (parent) {
+                parent = self.parent().closest(parent);
+            } else {
+                parent = self.parent();
+            }
+            var instance = self.data("stickyInstance");
 
-	$.fn.sticky._instances = [];
+            if (instance && options) {
+                if (typeof options === "object") {
+                    ret = $.extend(instance.options, options);
+                } else if (options === "options") {
+                    ret = instance.options;
+                } else if (typeof instance[options] === "function") {
+                    ret = instance[options].apply(instance, args.slice(1));
+                } else {
+                    throw new Error("Sticky Element has no option/method named " + method);
+                }
+            } else {
+                instance = new Sticky(self, parent, options || {});
+                self.data("stickyInstance", instance);
+                $.fn.sticky._instances.push(instance);
+            }
+        });
+        return ret || this;
+    };
 
-	$(window).on({
-		'resize': function() {
-		// Update the position/offset changed on resize and move
-			$.each($.fn.sticky._instances, function() {
-				this.update();
-			});
-		},
-		'scroll': function() {
-			// Move all those suckers on scroll
-			$.each($.fn.sticky._instances, function() {
-				if (!this._frozen) {
-					this.moveIt();
-				}
-			});
-		}
-	});
+    $.fn.sticky._instances = [];
 
-	// Deathstar death beam
-	$(document).on("pageleave", function () {
-		$(window).unbind('scroll resize');
-		$.fn.sticky._instances = [];
-	});
+    $(window).on({
+        'resize': function(e) {
+            // Update the position/offset changed on resize and move
+            $.each($.fn.sticky._instances, function() {
+                this.update();
+            });
+        },
+        'scroll': function() {
+            // Move all those suckers on scroll
+            $.each($.fn.sticky._instances, function() {
+                if (!this._frozen) {
+                    this.moveIt();
+                }
+            });
+        }
+    });
+
+    // Deathstar death beam
+    $(document).on("pageleave", function () {
+        $(window).unbind('scroll resize');
+        $.fn.sticky._instances = [];
+    });
 }(jQuery, window));
